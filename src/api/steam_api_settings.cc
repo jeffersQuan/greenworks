e6fc4fa1fbe92709ffe72826e372933901d3d8ae
xxx <xxx@xxx.com>
2019-05-16 10:15:04 +0800

    update

--- src/api/steam_api_settings.cc
+++ src/api/steam_api_settings.cc
@@ -477,7 +477,7 @@ NAN_METHOD(getProductsInfo) {
     if (rail_in_game_purchase_ == NULL) {
     	code = -3;
     } else {
-//    	if (rail_in_game_purchase_) {
+    	if (rail_in_game_purchase_) {
 //    		Local<Function> cb = info[0].As<v8::Function>();
 //    		if (iRailPurchaseStore == NULL) {
 //    		  iRailPurchaseStore = new RailPurchaseStore();
@@ -490,11 +490,11 @@ NAN_METHOD(getProductsInfo) {
 //    			code = 1;
 //    			ret = true;
 //    		} else {
-//    			code = -5;
+    			code = -5;
 //    		}
-//    	} else {
+    	} else {
     		code = -4;
-//    	}
+    	}
     }
   }
   else {
